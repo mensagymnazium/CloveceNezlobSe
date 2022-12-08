fa52b26ef2c800b24f921808348562233fdaaf6a
Robert Haken <haken@havit.cz>
2022-12-08 15:19:05 +0100

    nové strategie

--- CloveceNezlobSe/Program.cs
+++ CloveceNezlobSe/Program.cs
@@ -3,7 +3,11 @@ using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-var turnaj = new TurnajStrategii();
+var turnaj = new TurnajStrategii()
+{
+	VelikostHernihoPlanu = 40,
+	PocetHer = 1000
+};
 
 turnaj.PridejStrategii(new("HerniStrategieTahniPrvniMoznouFigurkou", hra => new HerniStrategieTahniPrvniMoznouFigurkou(hra)));
 turnaj.PridejStrategii(new("HerniStrategiePreferujVyhazovaniJinakPrvniMoznou", hra => new HerniStrategiePreferujVyhazovaniJinakPrvniMoznou(hra)));
@@ -11,5 +15,8 @@ turnaj.PridejStrategii(new("MartinF:HerniStrategieMartinF", hra => new CloveceNe
 turnaj.PridejStrategii(new("MarketaP:HerniStrategiePreferujDomecekPakVyhazovani", hra => new CloveceNezlobSe.MarketaP.HerniStrategiePreferujDomecekPakVyhazovani(hra)));
 turnaj.PridejStrategii(new("JanS:HerniStrategieHonza", hra => new CloveceNezlobSe.JanS.HerniStrategieHonza(hra)));
 turnaj.PridejStrategii(new("BenS:HerniStrategieBenjaminSwart", hra => new CloveceNezlobSe.BenS.HerniStrategieBenjaminSwart(hra)));
+turnaj.PridejStrategii(new("MartinF:HerniStrategieMartinFOptimized", hra => new CloveceNezlobSe.MartinF.HerniStrategieMartinFOptimized(hra, new CloveceNezlobSe.MartinF.HerniStrategieMartinFVahy())));
+turnaj.PridejStrategii(new("ViacheslavN:HerniStrategieNahodne", hra => new HerniStrategieNahodne(hra)));
+turnaj.PridejStrategii(new("ViacheslavN:HerniStrategieVyhodUtecJdi", hra => new HerniStrategieVyhodUtecJdi(hra)));
 
 turnaj.Start();
