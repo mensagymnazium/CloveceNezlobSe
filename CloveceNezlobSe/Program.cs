8aa4cd727e6f922f7c6fd30435486464e90cc5b4
Robert Haken <haken@havit.cz>
2022-12-08 12:45:50 +0100

    herní strategie BenS

--- CloveceNezlobSe/Program.cs
+++ CloveceNezlobSe/Program.cs
@@ -10,5 +10,6 @@ turnaj.PridejStrategii(new("HerniStrategiePreferujVyhazovaniJinakPrvniMoznou", h
 turnaj.PridejStrategii(new("MartinF:HerniStrategieMartinF", hra => new CloveceNezlobSe.MartinF.HerniStrategieMartinF(hra, new CloveceNezlobSe.MartinF.HerniStrategieMartinFVahy())));
 turnaj.PridejStrategii(new("MarketaP:HerniStrategiePreferujDomecekPakVyhazovani", hra => new CloveceNezlobSe.MarketaP.HerniStrategiePreferujDomecekPakVyhazovani(hra)));
 turnaj.PridejStrategii(new("JanS:HerniStrategieHonza", hra => new CloveceNezlobSe.JanS.HerniStrategieHonza(hra)));
+turnaj.PridejStrategii(new("BenS:HerniStrategieBenjaminSwart", hra => new CloveceNezlobSe.BenS.HerniStrategieBenjaminSwart(hra)));
 
 turnaj.Start();
