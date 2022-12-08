57dd6788609b273f7386e4b4401f088cffc1b502
Robert Haken <haken@havit.cz>
2022-12-08 16:54:31 +0100

    Vojta, Risa, Honza Sl

--- CloveceNezlobSe/Program.cs
+++ CloveceNezlobSe/Program.cs
@@ -13,18 +13,21 @@ var turnaj = new TurnajDvouher()
 
 turnaj.PridejStrategii(new("HerniStrategieTahniPrvniMoznouFigurkou", hra => new HerniStrategieTahniPrvniMoznouFigurkou(hra)));
 turnaj.PridejStrategii(new("HerniStrategiePreferujVyhazovaniJinakPrvniMoznou", hra => new HerniStrategiePreferujVyhazovaniJinakPrvniMoznou(hra)));
-turnaj.PridejStrategii(new("MartinF:HerniStrategieMartinF", hra => new CloveceNezlobSe.MartinF.HerniStrategieMartinF(hra, new CloveceNezlobSe.MartinF.HerniStrategieMartinFVahy())));
+//turnaj.PridejStrategii(new("MartinF:HerniStrategieMartinF", hra => new CloveceNezlobSe.MartinF.HerniStrategieMartinF(hra, new CloveceNezlobSe.MartinF.HerniStrategieMartinFVahy())));
 turnaj.PridejStrategii(new("MarketaP:HerniStrategiePreferujDomecekPakVyhazovani", hra => new CloveceNezlobSe.MarketaP.HerniStrategiePreferujDomecekPakVyhazovani(hra)));
 turnaj.PridejStrategii(new("JanS:HerniStrategieHonza", hra => new CloveceNezlobSe.JanS.HerniStrategieHonza(hra)));
 turnaj.PridejStrategii(new("BenS:HerniStrategieBenjaminSwart", hra => new CloveceNezlobSe.BenS.HerniStrategieBenjaminSwart(hra)));
 turnaj.PridejStrategii(new("MartinF:HerniStrategieMartinFOptimized", hra => new CloveceNezlobSe.MartinF.HerniStrategieMartinFOptimized(hra, new CloveceNezlobSe.MartinF.HerniStrategieMartinFVahy())));
 turnaj.PridejStrategii(new("ViacheslavN:HerniStrategieNahodne", hra => new HerniStrategieNahodne(hra)));
 turnaj.PridejStrategii(new("ViacheslavN:HerniStrategieVyhodUtecJdi", hra => new HerniStrategieVyhodUtecJdi(hra)));
-turnaj.PridejStrategii(new("ViacheslavN:HerniStrategiePoradSe", hra => new HerniStrategiePoradSe(hra)));
+//turnaj.PridejStrategii(new("ViacheslavN:HerniStrategiePoradSe", hra => new HerniStrategiePoradSe(hra)));
 turnaj.PridejStrategii(new("Jenda:MojeStrategieJenda", hra => new MojeStrategieJenda(hra)));
 turnaj.PridejStrategii(new("Jenda:MojeStrategieJenda2", hra => new MojeStrategieJenda2(hra)));
 turnaj.PridejStrategii(new("JonasH:HerniStrategieJonasH", hra => new HerniStrategieJonasH(hra)));
 turnaj.PridejStrategii(new("MaxJ:HerniStrategieMaxJAdvanced", hra => new HerniStrategieMaxJAdvanced(hra)));
+turnaj.PridejStrategii(new("VojtaT:HerniStrategieVojtechTvrdik", hra => new HerniStrategieVojtechTvrdik(hra)));
+turnaj.PridejStrategii(new("JanSl:StrategieSliva", hra => new StrategieSliva(hra)));
+turnaj.PridejStrategii(new("RisaD:HerniStrategieRisa", hra => new HerniStrategieRisa(hra)));
 
 //turnaj.PridejStrategii(new("Jenda:MojeStrategieJenda3", hra => new Jenda3(hra)));
 //turnaj.PridejStrategii(new("Jenda:StrategieShitty", hra => new StrategieShitty(hra)));
