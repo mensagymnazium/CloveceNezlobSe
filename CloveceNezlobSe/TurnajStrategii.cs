6dd102c7847c1c78a4ca85c24e322cf1e8fd5d3b
Viacheslav Nikiforov <71459428+Slavek592@users.noreply.github.com>
2022-12-08 19:31:53 +0100

    Turnaj trojher

--- CloveceNezlobSe/TurnajStrategii.cs
+++ CloveceNezlobSe/TurnajStrategii.cs
@@ -45,11 +45,58 @@ namespace CloveceNezlobSe
 				Console.WriteLine($"\t{vysledek.Key}: {vysledek.Value.PocetVitezstvi} vítězství ({vysledek.Value.VyhranychHer} vyhraných her)");
 			}
 		}
+	}
+	
+	public class TurnajovyVysledek
+	{
+		public int PocetVitezstvi { get; set; }
+		public int VyhranychHer { get; set; }
+	}
 
-		public class TurnajovyVysledek
+	public class TurnajTrojher : TurnajBase
+	{
+		public override void Start()
 		{
-			public int PocetVitezstvi { get; set; }
-			public int VyhranychHer { get; set; }
+			var celkoveVysledky = herniStrategie.ToDictionary(i => i.Name, i => new TurnajovyVysledek());
+
+			for (int i = 0; i < herniStrategie.Count; i++)
+			{
+				for (int j = i + 1; j < herniStrategie.Count; j++)
+				{
+					for (int k = j + 1; k < herniStrategie.Count; k++)
+					{
+						var strategie1 = herniStrategie[i];
+						var strategie2 = herniStrategie[j];
+						var strategie3 = herniStrategie[k];
+
+						var tester = new HerniStrategieTester();
+						var hraActivator = () =>
+						{
+							var hra = new Hra(new LinearniHerniPlan(this.VelikostHernihoPlanu));
+							hra.PridejHrace(new Hrac(strategie1.Name, strategie1.Activator(hra)));
+							hra.PridejHrace(new Hrac(strategie2.Name, strategie2.Activator(hra)));
+							hra.PridejHrace(new Hrac(strategie3.Name, strategie3.Activator(hra)));
+							return hra;
+						};
+
+						var vysledky = tester.Test(this.PocetHer, hraActivator);
+						var vitez = vysledky.OrderByDescending(i => i.Value).First();
+
+						celkoveVysledky[vitez.Key].PocetVitezstvi++;
+						foreach (var vysledek in vysledky)
+						{
+							celkoveVysledky[vysledek.Key].VyhranychHer =
+								celkoveVysledky[vysledek.Key].VyhranychHer + vysledek.Value;
+						}
+					}
+				}
+			}
+
+			Console.WriteLine("CELKOVÉ VÝSLEDKY:");
+			foreach (var vysledek in celkoveVysledky.OrderByDescending(i => i.Value.PocetVitezstvi).ThenByDescending(i => i.Value.VyhranychHer))
+			{
+				Console.WriteLine($"\t{vysledek.Key}: {vysledek.Value.PocetVitezstvi} vítězství ({vysledek.Value.VyhranychHer} vyhraných her)");
+			}
 		}
 	}
 }
