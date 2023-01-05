5f5a0e8aa421c8a127ff2d8c7dc44b3b1ebc9e5e
Xopabyteh <xopabyteh1337@gmail.com>
2023-01-05 17:08:48 +0100

    Added Žumpa
    Policko refactoring
    Vykresli is now virtual

--- CloveceNezlobSe/Policko.cs
+++ CloveceNezlobSe/Policko.cs
@@ -1,77 +1,76 @@
-﻿namespace CloveceNezlobSe
+﻿namespace CloveceNezlobSe;
+
+public class Policko
 {
-	public class Policko
-	{
-		HashSet<Figurka> figurkyNaPolicku = new();
-		bool dovolitViceFigurek;
-		private HerniPlan herniPlan;
+    protected HashSet<Figurka> figurkyNaPolicku = new();
+    protected bool dovolitViceFigurek;
+    protected HerniPlan herniPlan;
 
-		[Obsolete("Použij (policko is Domecek)")]
-		public bool JeDomecek => this is Domecek;
+    [Obsolete("Použij (policko is Domecek)")]
+    public bool JeDomecek => this is Domecek;
 
-		public Policko(HerniPlan herniPlan, bool dovolitViceFigurek = false)
-		{
-			this.dovolitViceFigurek = dovolitViceFigurek;
-            this.herniPlan = herniPlan;
+    public Policko(HerniPlan herniPlan, bool dovolitViceFigurek = false)
+    {
+        this.dovolitViceFigurek = dovolitViceFigurek;
+        this.herniPlan = herniPlan;
+    }
+
+    public virtual void PolozFigurku(Figurka figurka)
+    {
+        if (JeObsazeno())
+        {
+            throw new InvalidOperationException("Na políčku je již figurka a políčko nedovoluje více figurek.");
+        }
+        figurkyNaPolicku.Add(figurka);
+        figurka.NastavPolicko(this);
+    }
+
+    public Figurka ZvedniJedinouFigurku()
+    {
+        if (figurkyNaPolicku.Count != 1)
+        {
+            throw new InvalidOperationException("Na políčku není jediná figurka.");
+        }
+			
+        var figurka = figurkyNaPolicku.First();
+        ZvedniFigurku(figurka);
+
+        return figurka;
+    }
+
+    public void ZvedniFigurku(Figurka figurka)
+    {
+        if (!figurkyNaPolicku.Contains(figurka))
+        {
+            throw new InvalidOperationException("Na políčku figurka není.");
         }
 
-		public virtual void PolozFigurku(Figurka figurka)
-		{
-			if (JeObsazeno())
-			{
-				throw new InvalidOperationException("Na políčku je již figurka a políčko nedovoluje více figurek.");
-			}
-			figurkyNaPolicku.Add(figurka);
-			figurka.NastavPolicko(this);
-		}
+        figurkyNaPolicku.Remove(figurka);
+        figurka.NastavPolicko(null);
+    }
 
-		public Figurka ZvedniJedinouFigurku()
-		{
-			if (figurkyNaPolicku.Count != 1)
-			{
-				throw new InvalidOperationException("Na políčku není jediná figurka.");
-			}
-			
-			var figurka = figurkyNaPolicku.First();
-			ZvedniFigurku(figurka);
+    public IEnumerable<Figurka> ZjistiFigurkyHrace(Hrac hrac)
+    {
+        return figurkyNaPolicku.Where(figurka => figurka.Hrac == hrac);
+    }
 
-			return figurka;
-		}
+    public IEnumerable<Figurka> ZjistiFigurkyProtihracu(Hrac hrac)
+    {
+        return figurkyNaPolicku.Where(figurka => figurka.Hrac != hrac);
+    }
 
-		public void ZvedniFigurku(Figurka figurka)
-		{
-			if (!figurkyNaPolicku.Contains(figurka))
-			{
-				throw new InvalidOperationException("Na políčku figurka není.");
-			}
+    public bool JeObsazeno()
+    {
+        return !dovolitViceFigurek && (figurkyNaPolicku.Count != 0);
+    }
 
-			figurkyNaPolicku.Remove(figurka);
-			figurka.NastavPolicko(null);
-		}
-
-		public IEnumerable<Figurka> ZjistiFigurkyHrace(Hrac hrac)
-		{
-			return figurkyNaPolicku.Where(figurka => figurka.Hrac == hrac);
-		}
-
-		public IEnumerable<Figurka> ZjistiFigurkyProtihracu(Hrac hrac)
-		{
-			return figurkyNaPolicku.Where(figurka => figurka.Hrac != hrac);
-		}
-
-		public bool JeObsazeno()
-		{
-			return !dovolitViceFigurek && (figurkyNaPolicku.Count != 0);
-		}
-
-		public void Vykresli()
-		{
-			Console.Write("[");
-			foreach (var figurka in figurkyNaPolicku)
-			{
-				Console.Write(figurka.OznaceniFigurky);
-			}
-			Console.Write("]");
-		}
-	}
+    public virtual void Vykresli()
+    {
+        Console.Write("[");
+        foreach (var figurka in figurkyNaPolicku)
+        {
+            Console.Write(figurka.OznaceniFigurky);
+        }
+        Console.Write("]");
+    }
 }
---
+++ CloveceNezlobSe/Zumpa.cs
@@ -0,0 +1,18 @@
+﻿namespace CloveceNezlobSe;
+
+public class Zumpa : Policko
+{
+    public Zumpa(HerniPlan herniPlan, bool dovolitViceFigurek = false) : base(herniPlan, dovolitViceFigurek)
+    {
+    }
+
+    public override void PolozFigurku(Figurka figurka)
+    {
+        herniPlan.DejFigurkuNaStartovniPolicko(figurka);
+    }
+
+    public override void Vykresli()
+    {
+        Console.Write("< Žumpa >:( >");
+    }
+}
